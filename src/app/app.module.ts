30a6b46f7b93a0073f2647def38c3c76fda718b1
Clovis OLIVIER <clovis.olivier@gmail.com>
2018-08-29 19:55:23 +0200

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, 
+  MatCheckboxModule, 
+  MatToolbarModule,
+  MatInputModule,
+  MatSidenavModule
+} from '@angular/material';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatSidenavModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
